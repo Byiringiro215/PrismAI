5fa4cfb3abe7ee7997b55858338bbe7e17af9328
byiringiro215 <emmabyiringiro215@gmail.com>
2025-05-03 21:33:00 +0200

    Done changes about the ui

--- src/assets/Components/Intro.jsx
+++ src/assets/Components/Intro.jsx
@@ -6,7 +6,7 @@ import { ChevronDownIcon } from '@heroicons/react/20/solid';
 const Intro = () => {
   return (
     
-    <div className='bg-[##15192D] min-h-full flex flex-col justify-center text-center gap-[] px-2 md:px-[2rem] lg:px-[6rem] py-[10rem] brightness-50'>
+    <div className='bg-[##15192D] min-h-full flex flex-col justify-center text-center gap-[] px-2 md:px-[2rem] lg:px-[6rem] py-[10rem] brightness-50 shadow-xl'>
       <h1 className='font-extrabold text-[42px]  font-poppins leading-[3rem]  bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-purple-400'>Refracting Talents.</h1>
       <h1 className='font-extrabold text-[42px] text-white font-poppins leading-[3rem] '>Perfect Matches.</h1>
       <p className='text-[20px] text-white text-center font-semibold'>Make AI a job creator — not a job taker.</p>
--- src/assets/Components/Logo.jsx
+++ src/assets/Components/Logo.jsx
@@ -4,7 +4,7 @@ const Logo = () => {
   return (
     <div className='flex gap-[1rem] items-center'>
 <div className='rounded-full w-[40px] h-[40px] bg-purple-600 text-white py-[2px] px-[11px]'>
-      <span className='font-extrabold text-3xl '>P</span>
+      <span className='font-extrabold text-3xl font-sans '>P</span>
     </div>
   <span className='font-bold text-3xl bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-purple-400'>Prism</span>
     </div>
